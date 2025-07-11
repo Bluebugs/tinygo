c846d56ecb190fb4f7d30c444c87f50df35132ae
Ayke van Laethem <aykevanlaethem@gmail.com>
2025-07-11 09:34:44 +0200

    all: add Go 1.25 support

--- src/internal/abi/escape.go
+++ src/internal/abi/escape.go
@@ -8,3 +8,10 @@ import "unsafe"
 func NoEscape(p unsafe.Pointer) unsafe.Pointer {
 	return p
 }
+
+func Escape[T any](x T) T {
+	// This function is used from internal/synctest which doesn't seem to be
+	// used much by other packages.
+	// This probably needs support from the compiler to implement correctly.
+	panic("internal/abi.Escape: unimplemented")
+}
